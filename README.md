32b45944becb7dde5dab6389ffe39d219d4ebeaf
Valentin Agachi <github-com@agachi.name>
2017-07-02 21:15:25 +0200

    First version :tada:

---
+++ README.md
@@ -0,0 +1,3 @@
+# babel-plugin-inline-classnames
+
+[![Travis branch](https://img.shields.io/travis/avaly/babel-plugin-inline-classnames/master.svg?style=flat-square)](https://travis-ci.org/avaly/babel-plugin-inline-classnames)
---
+++ test/index.test.js
@@ -0,0 +1,25 @@
+/* eslint-env jest */
+/* eslint no-console: 0 */
+
+const babel = require('babel-core');
+const fs = require('fs');
+
+const babelOptions = {
+	plugins: ['syntax-jsx', __dirname + '/../index.js'],
+};
+
+[
+	'basic',
+	'custom-import-name',
+	'other-classnames',
+	'string-literal',
+	'object-expression',
+	'falsy-values',
+].forEach(test => {
+	it(test, () => {
+		const source = fs.readFileSync(__dirname + '/fixtures/' + test + '.js');
+		const { code } = babel.transform(source, babelOptions);
+
+		expect(code).toMatchSnapshot();
+	});
+});
